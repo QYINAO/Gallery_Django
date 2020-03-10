2cfd5935a5f044d0b9e36a45172444e1148045d3
QYINAO <13233481207@163.com>
2020-03-10 21:38:53 +0800

    模型类设计

--- Gallery_Django/settings.py
+++ Gallery_Django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gallery'
 ]
 
 MIDDLEWARE = [
@@ -75,12 +76,15 @@ WSGI_APPLICATION = 'Gallery_Django.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': "数据库名",
+         'HOST': '127.0.0.1',
+         'PORT': '3306',
+         'USER': 'root',
+         'PASSWORD': 'root'
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
---
+++ gallery/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+# 用户表
+class UserProfile(AbstractUser):
+    """用户"""
+    name = models.CharField(max_length=30, null=True, blank=True, verbose_name="姓名")
+    birthday = models.DateField(null=True, blank=True, verbose_name="出生年月")
+    gender = models.CharField(max_length=6,choices=(("male",u"男"),("female","女")), default="female", verbose_name="性别")
+    mobile = models.CharField(null=True, blank=True, max_length=11, verbose_name="电话")
+    email = models.EmailField(max_length=100, null=True, blank=True, verbose_name="邮箱")
+
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+
+# 作品
+class Works(models.Model):
+    title = models.CharField(default="",max_length=60,varbose_name="作品名称")    # 名称
+    describe = models.CharField(default="",max_length=200,varbose_name="作品描述")     # 描述
+    image = models.ImageField(varbose_name="图片",null=True, blank=True)
+
+    class Meta:
+        verbose_name = "作品"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
